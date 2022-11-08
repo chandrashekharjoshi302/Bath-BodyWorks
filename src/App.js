fc59def9f43241541a8a18797b9b8bf72fbd63ad
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2022-11-08 15:51:39 +0530

    Bath-BodyWorks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
-import './App.css';
-
+import MainRoutes from "./Routes/MainRoutes";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      
+       <Navbar/>
+      <MainRoutes/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
-
-
